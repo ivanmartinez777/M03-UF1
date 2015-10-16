40d6f52d790e745e67e648a99bd9e0fa0656df03
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-16 16:39:27 +0200

    ordenaymuestraIvanv.2.0

--- UF1-Ra1/Ordenaymuestra.vb
+++ UF1-Ra1/Ordenaymuestra.vb
@@ -12,31 +12,32 @@
         If n1 < n2 And n2 < n3 Then
 
             result = "          " + n1.ToString() + "          " + n2.ToString + "          " + n3.ToString
-            MessageBox.Show(result)
+
+            '123
         ElseIf n1 < n3 And n3 < n2 Then
             result = "          " + n1.ToString() + "          " + n3.ToString + "          " + n2.ToString
             '132
-            MessageBox.Show(result)
+
         ElseIf n2 < n1 And n1 < n3 Then
             result = "          " + n2.ToString() + "          " + n1.ToString + "          " + n3.ToString
             '213
-            MessageBox.Show(result)
+
         ElseIf n3 < n1 And n1 < n2 Then
             result = "          " + n3.ToString() + "          " + n1.ToString + "          " + n2.ToString
             '231
-            MessageBox.Show(result)
+
         ElseIf n2 < n3 And n3 < n1 Then
             result = "          " + n2.ToString() + "          " + n3.ToString + "          " + n1.ToString
             '312
-            MessageBox.Show(result)
+
 
         Else
 
             result = "          " + n3.ToString() + "          " + n2.ToString + "          " + n1.ToString
             '321
-            MessageBox.Show(result)
+
 
         End If
-
+        MessageBox.Show(result)
     End Sub
 End Class
