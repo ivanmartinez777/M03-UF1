14d76bce073d62b486a4369bc9283bc545d04e10
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-16 18:36:26 +0200

    Ejercicio 2

--- UF1-Ra1/Ejercicio_2.vb
+++ UF1-Ra1/Ejercicio_2.vb
@@ -2,14 +2,14 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim n1 As Integer
         Dim n2 As Integer
-        Dim r As Integer
 
-        Dim expresion As Boolean
+
+
 
         n1 = CInt(TextBox1.Text)
         n2 = CInt(TextBox2.Text)
 
-        If expresion = n1 / n2 = r Then
+        If n1 Mod n2 = 0 Then
             MessageBox.Show("Es divisible")
         Else
             MessageBox.Show("No es divisible")
