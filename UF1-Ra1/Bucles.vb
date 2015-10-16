e757335f9ebc97c7b039d66bd463bd9a4efe500d
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-16 17:13:22 +0200

    tablademultiplicarBucle

---
+++ UF1-Ra1/Bucles.vb
@@ -0,0 +1,3 @@
+﻿Public Class Bucles
+
+End Class
---
+++ UF1-Ra1/tablamultiplicar.vb
@@ -0,0 +1,22 @@
+﻿Public Class tablamultiplicar
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n1 As Integer
+        Dim salida As String = ""
+        Dim cont As Integer
+        Dim parcial As Integer
+
+        n1 = TextBox1.Text()
+
+
+        n1 = TextBox1.Text()
+
+        For cont = 0 To 10
+            parcial = n1 * cont
+            salida = salida + n1.ToString() + "x" + cont.ToString + "=" + parcial.ToString() + Environment.NewLine
+        Next
+
+        MessageBox.Show(salida)
+
+
+    End Sub
+End Class
