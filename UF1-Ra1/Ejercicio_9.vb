93315609545c67e43daffb87428b09cb2a973f00
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-20 14:09:55 +0200

    ejercicio_9 sa2

--- UF1-Ra1/Ejercicio_9.vb
+++ UF1-Ra1/Ejercicio_9.vb
@@ -7,10 +7,20 @@
         mes = CInt(TextBox1.Text)
         año = CInt(TextBox2.Text)
 
-        If mes Mod 2 <> 0 And mes < 7 Then
+        If ((mes = 2) And (año Mod 4 = 0) And (año Mod 100 = 0) And (año Mod 400 <> 0)) Then
+            dias = 29
+        ElseIf ((mes = 2) And (año Mod 4 = 0) And (año Mod 100 = 0)) Then
+            dias = 28
+        ElseIf ((mes = 2) And (mes Mod 4 = 0)) Then
+            dias = 29
+        ElseIf mes = 2 Then
+            dias = 28
+        ElseIf mes <> 2 And mes Mod 2 <> 0 And mes < 7 Then
             dias = 31
         Else
             dias = 30
+
+
 
         End If
         Label1.Text = dias
