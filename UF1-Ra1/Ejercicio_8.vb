83b403fff10f12a0dfed998231dea96bbf66cf21
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-20 13:05:24 +0200

    Ejercicio_8

---
+++ UF1-Ra1/Ejercicio_8.vb
@@ -0,0 +1,27 @@
+﻿Public Class Ejercicio_8
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nota As Integer
+        Dim resultado As String
+
+        nota = CInt(TextBox1.Text)
+
+
+        Select Case nota
+            Case 0 To 4
+                resultado = "S"
+            Case 5, 6
+                resultado = "A"
+            Case 7, 8
+                resultado = "N"
+            Case 9
+                resultado = "E"
+            Case 10
+                resultado = "M"
+
+
+        End Select
+
+        Label1.Text = resultado
+
+    End Sub
+End Class
