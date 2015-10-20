2decae7311f377b145be8edf32672a53d6c8d08a
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-20 13:42:31 +0200

    ejercicio_9 sa

---
+++ UF1-Ra1/Ejercicio_9.vb
@@ -0,0 +1,18 @@
+﻿Public Class Ejercicio_9
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim mes As Integer
+        Dim año As Integer
+        Dim dias As Integer
+
+        mes = CInt(TextBox1.Text)
+        año = CInt(TextBox2.Text)
+
+        If mes Mod 2 <> 0 And mes < 7 Then
+            dias = 31
+        Else
+            dias = 30
+
+        End If
+        Label1.Text = dias
+    End Sub
+End Class
--- UF1-Ra1/My Project/Application.Designer.vb
+++ UF1-Ra1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.UF1_Ra1.Ejercicio_8
+            Me.MainForm = Global.UF1_Ra1.Ejercicio_9
         End Sub
     End Class
 End Namespace
