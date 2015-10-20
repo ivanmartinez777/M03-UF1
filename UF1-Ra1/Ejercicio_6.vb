6d39c804762d815a747f96dde82b7fe6ab9af83c
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-20 12:33:03 +0200

    Ejercicio_6

---
+++ UF1-Ra1/Ejercicio_6.vb
@@ -0,0 +1,28 @@
+﻿Public Class Ejercicio_6
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ndia As Integer
+        Dim dia As String
+
+        ndia = CInt(TextBox1.Text)
+
+        Select Case ndia
+
+            Case 1
+                dia = "Dilluns"
+            Case 2
+                dia = "Dimarts"
+            Case 3
+                dia = "Dimecres"
+            Case 4
+                dia = "Dijous"
+            Case 5
+                dia = "Divendres"
+            Case 6
+                dia = "Dissabte"
+            Case 7
+                dia = "Diumenge"
+        End Select
+
+        Label1.Text = dia
+    End Sub
+End Class
