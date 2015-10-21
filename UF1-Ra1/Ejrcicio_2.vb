6a0ec861951db7abeb8a9a3c7e69a7e58671d69f
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-21 17:23:34 +0200

    Ejercicio 2 modificado usando InStr

---
+++ UF1-Ra1/Ejrcicio_2.vb
@@ -0,0 +1,18 @@
+﻿Public Class Ejrcicio_2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim posicion As Integer
+        Dim letra As String
+
+        letra = TextBox1.Text
+
+        posicion = InStr("aeioubcdfghjklmnñopqrstvwxyz", letra)
+
+        If posicion < 5 And posicion > 0 Then
+            MessageBox.Show("vocal")
+        ElseIf posicion > 5 Then
+            MessageBox.Show("consonante")
+        Else
+            MessageBox.Show("error")
+        End If
+    End Sub
+End Class
