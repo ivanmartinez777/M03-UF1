c52e4fb7375e2cbd65e0e757a496e7e3256d80c5
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-10-21 17:53:46 +0200

    Ejercicio_9_y_4

--- UF1-Ra1/Ejercicio_4.Designer.vb
+++ UF1-Ra1/Ejercicio_4.Designer.vb
@@ -28,7 +28,7 @@ Partial Class Ejercicio_4
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(39, 112)
+        Me.Button1.Location = New System.Drawing.Point(42, 81)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -37,7 +37,7 @@ Partial Class Ejercicio_4
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(149, 114)
+        Me.TextBox1.Location = New System.Drawing.Point(169, 83)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 1
--- UF1-Ra1/Ejercicio_4.vb
+++ UF1-Ra1/Ejercicio_4.vb
@@ -1,73 +1,18 @@
 ﻿Public Class Ejercicio_4
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim caracter As String
-        Dim Result As String
-        caracter = TextBox1.Text
+        Dim posicion As Integer
+        Dim letra As String
 
-        If caracter = "a" Then
-            Result = "vocal"
-        ElseIf caracter = "e" Then
-            Result = "vocal"
-        ElseIf caracter = "i" Then
-            Result = "vocal"
-        ElseIf caracter = "o" Then
-            Result = "vocal"
-        ElseIf caracter = "u" Then
-            Result = "vocal"
-        ElseIf caracter = "b" Then
-            Result = "consonante"
-        ElseIf caracter = "c" Then
-            Result = "consonante"
-        ElseIf caracter = "d" Then
-            Result = "consonante"
-        ElseIf caracter = "f" Then
-            Result = "consonante"
-        ElseIf caracter = "g" Then
-            Result = "consonante"
-        ElseIf caracter = "h" Then
-            Result = "consonante"
-        ElseIf caracter = "j" Then
-            Result = "consonante"
-        ElseIf caracter = "k" Then
-            Result = "consonante"
-        ElseIf caracter = "l" Then
-            Result = "consonante"
-        ElseIf caracter = "m" Then
-            Result = "consonante"
-        ElseIf caracter = "n" Then
-            Result = "consonante"
-        ElseIf caracter = "ñ" Then
-            Result = "consonante"
-        ElseIf caracter = "p" Then
-            Result = "consonante"
-        ElseIf caracter = "q" Then
-            Result = "consonante"
-        ElseIf caracter = "r" Then
-            Result = "consonante"
-        ElseIf caracter = "s" Then
-            Result = "consonante"
-        ElseIf caracter = "t" Then
-            Result = "consonante"
-        ElseIf caracter = "v" Then
-            Result = "consonante"
-        ElseIf caracter = "w" Then
-            Result = "consonante"
-        ElseIf caracter = "x" Then
-            Result = "consonante"
-        ElseIf caracter = "y" Then
-            Result = "consonante"
-        ElseIf caracter = "z" Then
-            Result = "consonante"
+        letra = TextBox1.Text
+
+        posicion = InStr("aeioubcdfghjklmnñopqrstvwxyz", letra)
+
+        If posicion < 5 And posicion > 0 Then
+            MessageBox.Show("vocal")
+        ElseIf posicion > 5 Then
+            MessageBox.Show("consonante")
         Else
-            Result = "error"
+            MessageBox.Show("error")
         End If
-        MessageBox.Show(Result)
-
-
-
-    End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
     End Sub
 End Class
