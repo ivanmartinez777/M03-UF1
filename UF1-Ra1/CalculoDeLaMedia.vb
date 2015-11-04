efcb466b892f0ef49f0686c59aa09d1d5b06959c
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-11-04 18:01:11 +0100

    04-11-15 v.1

---
+++ UF1-Ra1/CalculoDeLaMedia.vb
@@ -0,0 +1,23 @@
+﻿Public Class CalculoDeLaMedia
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nota As Integer
+        Dim sumaTotal As Double = 0
+        Dim cnt As Integer
+        Dim media As Double
+
+
+
+        For cnt = 0 To 9
+            'ponemos de 0 a 9 porque serán 10 notas
+
+            nota = InputBox("introduce la nota")
+            sumaTotal = sumaTotal + nota
+        Next
+
+        media = sumaTotal / 10
+
+        MessageBox.Show(media.ToString)
+
+
+    End Sub
+End Class
---
+++ UF1-Ra1/IntroducciónYMenor.vb
@@ -0,0 +1,23 @@
+﻿Public Class IntroducciónYMenor
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim numero As String
+        Dim cnt As Integer
+        Dim menor As Integer = 0
+
+        numero = InputBox("introduce el numero")
+        menor = numero
+
+        For cnt = 0 To 8
+            numero = InputBox("introduce el numero")
+            If numero < menor Then
+                menor = numero
+
+
+            End If
+
+
+        Next
+        MessageBox.Show(menor.ToString)
+
+    End Sub
+End Class
