706c488c1e13beaf00692046ccd6e2a0869214a7
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-11-11 18:10:27 +0100

    11-11-15 v.1

---
+++ UF1-Ra1/Factorizacion.vb
@@ -0,0 +1,16 @@
+﻿Public Class Factorizacion
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim cnt As Integer
+        Dim Resultado As Integer = 1
+        Dim Numero As Integer
+
+        Numero = InputBox("introduce un numero para la factorizacion")
+
+        For cnt = 1 To Numero
+            Resultado = Resultado * (cnt)
+
+            MessageBox.Show(Resultado)
+        Next
+
+    End Sub
+End Class
--- UF1-Ra1/My Project/Application.Designer.vb
+++ UF1-Ra1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.UF1_Ra1.triangulo
+            Me.MainForm = Global.UF1_Ra1.Factorizacion
         End Sub
     End Class
 End Namespace
