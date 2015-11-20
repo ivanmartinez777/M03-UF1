9de6381aa766cfd81f02625c6b03629863b57757
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-11-20 17:42:10 +0100

    20-11-15 v.1

---
+++ UF1-Ra1/Ejercicio_14.vb
@@ -0,0 +1,17 @@
+﻿Public Class Ejercicio_14
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Integer = 1
+        Dim a As Integer = 0
+
+        While i <= 20
+            i = i + 2
+            Do While a < 10
+                MessageBox.Show("ivan mola")
+                a = a + 1
+            Loop
+
+        End While
+
+
+    End Sub
+End Class
