be672c81f838c6cbc0be9dacfbd2c94471263e1e
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-11-26 11:02:16 +0100

    26-11-15 v.2

---
+++ UF1-Ra1/AdivinarUnNumero2.vb
@@ -0,0 +1,33 @@
+﻿Public Class AdivinarUnNumero2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim valor As Integer
+        Dim acierto As Integer
+        Dim cnt As Integer = 4
+
+        acierto = 4
+
+        Do Until valor = acierto Or cnt = 0
+            valor = InputBox("introduce un número amigo")
+            cnt = cnt - 1
+            If valor = acierto Then
+                MessageBox.Show("you win!")
+
+            ElseIf valor <> acierto And cnt > 0
+                MessageBox.Show(cnt)
+
+            End If
+
+        Loop
+
+        If cnt > 0 Then
+            MessageBox.Show("esta vez lo has conseguido, pero no volverá a ocurrir!")
+
+        Else
+            MessageBox.Show("Se te acabaron los intentos amigo. Estas acabado!")
+
+        End If
+
+
+
+    End Sub
+End Class
--- UF1-Ra1/My Project/Application.Designer.vb
+++ UF1-Ra1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.UF1_Ra1.SumarIntervalos
+            Me.MainForm = Global.UF1_Ra1.AdivinarUnNumero2
         End Sub
     End Class
 End Namespace
