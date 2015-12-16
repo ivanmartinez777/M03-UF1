67dc6d65ee46388db55f9eff68b8920e23d6b048
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-12-16 18:19:55 +0100

    16-12-15 v.4

--- UF1-Ra1/My Project/Application.Designer.vb
+++ UF1-Ra1/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.UF1_Ra1.Ejercicio1
+            Me.MainForm = Global.UF1_Ra1.Boletin5
         End Sub
     End Class
 End Namespace
