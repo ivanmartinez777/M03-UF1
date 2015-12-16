ba3af34d61604da3985294f5c68922005988706d
ivanmartinez777 <ivanmartinez777@hotmail.com>
2015-12-16 17:17:53 +0100

    16-12-15

---
+++ UF1-Ra1/Boletin5.vb
@@ -0,0 +1,12 @@
+﻿Public Class Boletin5
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tabla(9, 9) As Integer
+        Dim i, j As Integer
+
+        For i = 0 To 9
+            For j = 0 To 9
+
+            Next
+        Next
+    End Sub
+End Class
---
+++ UF1-Ra1/Ejercicio1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Ejercicio1
+    Dim tabla(149, 149) As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim i, j As Integer
+        Dim r As New Random
+        Dim salida As String
+
+        For i = 0 To 149
+            For j = 0 To 149
+                tabla(i, j) = r.Next(0, 100)
+                salida = salida + tabla(i, j).ToString + " "
+            Next
+            salida = salida + vbNewLine
+        Next
+        MessageBox.Show(salida)
+    End Sub
+End Class
