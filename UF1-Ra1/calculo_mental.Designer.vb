ed5afdfd2c458b53e76634110ea4e94c8d614dd4
ivanmartinez777 <ivanmartinez777@hotmail.com>
2016-03-11 19:16:07 +0100

    11/03/16 v.4

--- UF1-Ra1/calculo_mental.Designer.vb
+++ UF1-Ra1/calculo_mental.Designer.vb
@@ -29,6 +29,17 @@ Partial Class calculo_mental
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.timerr1 = New System.Windows.Forms.Label()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -37,7 +48,7 @@ Partial Class calculo_mental
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Iniciar"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox1
@@ -49,11 +60,11 @@ Partial Class calculo_mental
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(194, 214)
+        Me.Button2.Location = New System.Drawing.Point(174, 209)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(27, 23)
+        Me.Button2.Size = New System.Drawing.Size(64, 23)
         Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "Aceptar"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Timer1
@@ -76,11 +87,107 @@ Partial Class calculo_mental
         Me.TextBox2.Size = New System.Drawing.Size(100, 20)
         Me.TextBox2.TabIndex = 6
         '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(13, 79)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(45, 17)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "facil"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(13, 102)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(59, 17)
+        Me.CheckBox2.TabIndex = 8
+        Me.CheckBox2.Text = "Normal"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 126)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(51, 17)
+        Me.CheckBox3.TabIndex = 9
+        Me.CheckBox3.Text = "Dificil"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 1000
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(13, 226)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "salir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(160, 111)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(21, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(248, 95)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Incorrectas"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(251, 111)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(25, 20)
+        Me.TextBox4.TabIndex = 14
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(157, 95)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Correctas"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 183)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 16
+        '
         'calculo_mental
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(348, 261)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.timerr1)
         Me.Controls.Add(Me.Button2)
@@ -99,4 +206,15 @@ Partial Class calculo_mental
     Friend WithEvents Timer1 As Timer
     Friend WithEvents timerr1 As Label
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
--- UF1-Ra1/calculo_mental.vb
+++ UF1-Ra1/calculo_mental.vb
@@ -4,7 +4,7 @@
     Dim op As Integer
     Dim op2 As Integer
     Dim pregunta As Integer
-    Dim count As Integer = 10
+    Dim count As Integer = 0
     Dim cp As Integer = 0
     Dim cc As Integer = 0
     Dim ci As Integer = 0
@@ -14,8 +14,43 @@
 
     Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+        If (CheckBox1.Checked = True And CheckBox2.Checked = True) Or (CheckBox1.Checked = True And CheckBox3.Checked = True) Or (CheckBox3.Checked = True And CheckBox2.Checked = True) Then
+            MessageBox.Show("Sólo puede seleccionar un nivel")
 
-        Timer1.Start()
+        ElseIf CheckBox1.Checked = True Then
+            Label3.Text = ""
+            TextBox3.Text = ""
+            TextBox4.Text = ""
+            Timer1.Start()
+            count = 10
+            CheckBox1.Visible = False
+            CheckBox2.Visible = False
+            CheckBox3.Visible = False
+            Button1.Visible = False
+        ElseIf CheckBox2.Checked = True Then
+            Timer2.Start()
+            Label3.Text = ""
+            TextBox3.Text = ""
+            TextBox4.Text = ""
+            count = 20
+            CheckBox1.Visible = False
+            CheckBox2.Visible = False
+            CheckBox3.Visible = False
+            Button1.Visible = False
+        ElseIf CheckBox3.Checked = True Then
+            Label3.Text = ""
+            TextBox3.Text = ""
+            TextBox4.Text = ""
+            Timer3.Start()
+            count = 30
+            CheckBox1.Visible = False
+            CheckBox2.Visible = False
+            CheckBox3.Visible = False
+            Button1.Visible = False
+
+        Else
+            MessageBox.Show("selecciona una dificultad por favor")
+        End If
 
 
 
@@ -37,13 +72,13 @@
         If count = 10 Then
 
                 pregunta = r.Next(1, 3)
-                op = r.Next(10, 100)
+            op = r.Next(1, 100)
 
-                op2 = r.Next(10, 100)
+            op2 = r.Next(1, 100)
 
 
 
-                Select Case pregunta
+            Select Case pregunta
                     Case 1
                         pregunta = op - op2
                         TextBox1.Text = op.ToString + " - " + op2.ToString
@@ -77,12 +112,13 @@
                     juicio = False
                 End If
             If juicio = True Then
-                MessageBox.Show("tienes razón  ")
-                cc = cc + 1
-            Else
-                MessageBox.Show("no sabes lo que dices")
-                ci = ci + 1
 
+                cc = cc + 1
+                TextBox3.Text = cc
+            Else
+
+                ci = ci + 1
+                TextBox4.Text = ci
             End If
             cp = cp + 1
             TextBox2.Text = ""
@@ -92,20 +128,25 @@
 
 
         If cp = 5 Then
-            MessageBox.Show("Se acabó")
+
 
             TextBox2.Text = ""
             TextBox1.Text = ""
             Timer1.Enabled = False
             cp = 0
             If ci < cc Then
-                MessageBox.Show("Eres un maestro, has aprobado. Has cometido " + ci.ToString + " errores y " + cc.ToString + " aciertos.")
+                Label3.Text = "Aprobado"
             Else
-                MessageBox.Show("Eres un zote, has suspendido. Has cometido " + ci.ToString + " errores y " + cc.ToString + " aciertos.")
+                Label3.Text = "Suspedido"
             End If
             ci = 0
             cc = 0
             cp = 0
+
+            CheckBox1.Visible = True
+            CheckBox2.Visible = True
+            CheckBox3.Visible = True
+            Button1.Visible = True
 
         Else
             Timer1.Enabled = True
@@ -115,6 +156,202 @@
 
     End Sub
 
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If count = 20 Then
+
+            pregunta = r.Next(1, 5)
+            op = r.Next(100, 1000)
+
+            op2 = r.Next(100, 1000)
 
 
+
+            Select Case pregunta
+                Case 1
+                    pregunta = op - op2
+                    TextBox1.Text = op.ToString + " - " + op2.ToString
+
+
+                Case 2
+                    pregunta = op + op2
+                    TextBox1.Text = op.ToString + " + " + op2.ToString
+                Case 3
+                    pregunta = op * op2
+                    TextBox1.Text = op.ToString + " * " + op2.ToString
+                Case 4
+                    If op > op2 Then
+                        pregunta = op / op2
+                    Else
+                        pregunta = op2 / op
+                    End If
+
+
+                    TextBox1.Text = op.ToString + " / " + op2.ToString
+            End Select
+
+        End If
+        Timer2.Enabled = True
+        count = count - 1
+        timerr1.Text = count.ToString
+
+        If count < 1 Then
+            count = 0
+            Timer2.Enabled = False
+        End If
+        If count = 0 Then
+            If TextBox2.Text = "" Then
+                resp = 0
+            Else
+                resp = TextBox2.Text
+            End If
+            If pregunta = resp Then
+                juicio = True
+            Else
+                juicio = False
+            End If
+            If juicio = True Then
+
+                cc = cc + 1
+                TextBox3.Text = cc
+            Else
+
+                ci = ci + 1
+                TextBox4.Text = ci
+            End If
+            cp = cp + 1
+            TextBox2.Text = ""
+            count = 20
+        End If
+
+
+
+        If cp = 5 Then
+
+
+            TextBox2.Text = ""
+            TextBox1.Text = ""
+            Timer2.Enabled = False
+            cp = 0
+            If ci < cc Then
+                Label3.Text = "Aprobado"
+            Else
+                Label3.Text = "Suspedido"
+            End If
+            ci = 0
+            cc = 0
+            cp = 0
+
+            CheckBox1.Visible = True
+            CheckBox2.Visible = True
+            CheckBox3.Visible = True
+            Button1.Visible = True
+        Else
+            Timer2.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+
+        If count = 30 Then
+
+            pregunta = r.Next(1, 5)
+            op = r.Next(1000, 10000)
+
+            op2 = r.Next(1000, 10000)
+
+
+
+            Select Case pregunta
+                Case 1
+                    pregunta = op - op2
+                    TextBox1.Text = op.ToString + " - " + op2.ToString
+
+                Case 2
+                    pregunta = op + op2
+                    TextBox1.Text = op.ToString + " + " + op2.ToString
+                Case 3
+                    pregunta = op * op2
+                    TextBox1.Text = op.ToString + " * " + op2.ToString
+                Case 4
+                    If op > op2 Then
+                        pregunta = op / op2
+                    Else
+                        pregunta = op2 / op
+                    End If
+            End Select
+
+        End If
+        Timer3.Enabled = True
+        count = count - 1
+        timerr1.Text = count.ToString
+
+        If count < 1 Then
+            count = 0
+            Timer3.Enabled = False
+        End If
+        If count = 0 Then
+            If TextBox2.Text = "" Then
+                resp = 0
+            Else
+                resp = TextBox2.Text
+            End If
+            If pregunta = resp Then
+                juicio = True
+            Else
+                juicio = False
+            End If
+            If juicio = True Then
+
+                cc = cc + 1
+                TextBox3.Text = cc
+            Else
+
+                ci = ci + 1
+                TextBox4.Text = ci
+            End If
+            cp = cp + 1
+            TextBox2.Text = ""
+            count = 30
+        End If
+
+
+
+        If cp = 5 Then
+
+
+            TextBox2.Text = ""
+            TextBox1.Text = ""
+            Timer3.Enabled = False
+            cp = 0
+            If ci < cc Then
+                Label3.Text = "Aprobado"
+            Else
+                Label3.Text = "Suspedido"
+            End If
+            ci = 0
+            cc = 0
+            cp = 0
+
+            CheckBox1.Visible = True
+            CheckBox2.Visible = True
+            CheckBox3.Visible = True
+            Button1.Visible = True
+        Else
+            Timer3.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
 End Class
